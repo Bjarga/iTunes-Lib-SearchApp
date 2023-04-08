02d1a109dacb03f2e1625e180945426b19ad5e01
unknown <bjarga@gmail.com>
2023-04-08 13:05:27 +0200

    Add Railway configuration for backend only

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-// This code starts a Node.js server using the "app" module and 
-// listens for incoming HTTP requests on port 5000.
-const app = require('./app');
-
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
-});
